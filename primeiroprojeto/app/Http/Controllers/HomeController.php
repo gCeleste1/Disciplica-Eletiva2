ca521592555aae721c75fde35106caa5d4a33ec7
Gabriel Celeste Silva <gabriel.celeste17@gmail.com>
2022-02-22 19:55:20 -0300

    Commit depois do bootstrap configurado

--- primeiroprojeto/app/Http/Controllers/HomeController.php
+++ primeiroprojeto/app/Http/Controllers/HomeController.php
@@ -8,6 +8,6 @@ class HomeController extends Controller
 {
     public function  index ()
     {
-        return view ("Welcome");
+        return view ("welcome");
     }
 }
--- primeiroprojeto/routes/web.php
+++ primeiroprojeto/routes/web.php
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/index', [HomeController::class,  'index']);
+Route::get('/index', [HomeController::class, 'index']);
